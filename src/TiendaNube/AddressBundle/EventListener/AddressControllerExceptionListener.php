85758a6d7e531a2531224ee2b54586c99f274cb3
nsiksnys <nadiasiksnys@gmail.com>
2018-04-25 21:55:19 -0300

    configuración de servicios

---
+++ src/TiendaNube/AddressBundle/EventListener/AddressControllerExceptionListener.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\TiendaNube\AddressBundle\EventListener;
+
+use Psr\Log\LoggerInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpKernel\Event\GetResponseForExceptionEvent;
+
+class ClientControllerExceptionListener
+{
+    private $code = 500;
+    private $message = "Something went wrong!";
+
+    public function __construct(LoggerInterface $logger)
+    {
+        $this->logger = $logger;
+    }
+    
+    public function onKernelException(GetResponseForExceptionEvent $event)
+    {
+        $exception = $event->getException();
+
+        if (!$exception instanceof \Exception) {
+            return;
+        }
+        
+        $this->logger->err($exception->getMessage());
+        $code = 500;
+
+        $responseData = [
+            'error' => [
+                'code' => $this->code,
+                'message' => $this->message
+            ]
+        ];
+
+        $event->setResponse(new JsonResponse($responseData, $code));
+    }
+}
