e7e73d7856f83eebb0a632b0af909660f2202e46
nsiksnys <nadiasiksnys@gmail.com>
2018-04-26 00:07:17 -0300

    Territorios - State

---
+++ src/TiendaNube/TerritoryBundle/Entity/State.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\TiendaNube\TerritoryBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="territory_state")
+ */
+class State extends Territory
+{
+    /**
+     * The state acronym
+     *
+     * @var string
+     * @ORM\Column(type="string", nullable=false)
+     * @Assert\Type("string")
+     */
+    protected $acronym;
+
+    public function getType()
+    {
+        return $this::TYPE_STATE;
+    }
+
+    /**
+     * @return string
+     */
+    public function getAcronym()
+    {
+        return $this->acronym;
+    }
+
+    /**
+     * @param string $acronym
+     *
+     * @return this
+     */
+    public function setAcronym($acronym)
+    {
+        $this->acronym = $acronym;
+
+        return $this;
+    }
+
+    public function toJson()
+    {
+        return [
+            "acronym" => $this->acronym;
+        ];
+    }
+}
