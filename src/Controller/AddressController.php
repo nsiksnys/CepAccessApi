e566829eece6059db89e6286bda0c01f414f7c39
nsiksnys <nadiasiksnys@gmail.com>
2018-04-28 23:59:31 -0300

    Limpieza
    Elimino uses de clases que no se usan.

--- src/Controller/AddressController.php
+++ src/Controller/AddressController.php
@@ -6,10 +6,10 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
-use Symfony\Component\Security\Core\Security;
 
 use App\Provider\AddressProvider;
 
