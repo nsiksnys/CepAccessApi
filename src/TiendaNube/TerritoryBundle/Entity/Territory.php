3a2479aff47a6d5f99c99cc6684fe6190106ee77
nsiksnys <nadiasiksnys@gmail.com>
2018-04-28 23:58:57 -0300

    Limpieza
    Elimino uses de clases que no se usan.

--- src/TiendaNube/TerritoryBundle/Entity/Territory.php
+++ src/TiendaNube/TerritoryBundle/Entity/Territory.php
@@ -6,7 +6,6 @@ use Doctrine\ORM\Mapping as ORM;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Criteria;
 use Symfony\Component\Validator\Constraints as Assert;
-use Symfony\Component\Serializer\Annotation\Groups;
 
 /**
  * @ORM\Entity
